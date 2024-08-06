2dfefe6c072f3690e799fbaf28977bea5dd6753e
Kate Belisle <121182800+ulc0@users.noreply.github.com>
2024-08-06 12:33:17 -0400

    remove python version check

--- python_bindings/setup.py
+++ python_bindings/setup.py
@@ -14,9 +14,9 @@ if sys.platform.startswith("win") and struct.calcsize("P") * 8 == 32:
 dep_list = ['pybind11>=2.2.3', 'psutil']
 dep_list.append("numpy>=1.10.0 ; python_version>='3.5'")
 
-py_version = tuple([int(s) for s in platform.python_version().split('.')])[0:2]
-if py_version != (2, 7) and py_version < (3, 5):
-    raise RuntimeError("Python version 2.7 or >=3.5 required.")
+#py_version = tuple([int(s) for s in platform.python_version().split('.')])[0:2]
+#if py_version != (2, 7) and py_version < (3, 5):
+#    raise RuntimeError("Python version 2.7 or >=3.5 required.")
 
 print('Dependence list:', dep_list)
 
